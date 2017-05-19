768586036714ea8ebe29cf685d137ed47b8010d7
MechaMustang6485 <lamathesonrobotics@gmail.com>
2017-05-18 17:27:22 -0700

    Add files via upload

---
+++ TODO_LIST.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team6485.robot;
+
+public class TODO_LIST {
+
+  /*
+   * TODO Work on encoders for drive train and bridge.
+   */
+
+  /*
+   * TODO Check what PWM rate the RoboRio reports when a Bridge switch is overriding the controller.
+   */
+
+}
